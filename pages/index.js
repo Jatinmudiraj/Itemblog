85ef0bffd0164bb0a0c93f6627f0d6b1b07ebb80
Jatinmudiraj <jatinmudiraj126@gmail.com>
2024-02-27 12:10:51 +0530

    <commit_messae>

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
-import {Product} from "@/models/Product";
-import {mongooseConnect} from "@/lib/mongoose";
 import NewProducts from "@/components/NewProducts";
 import Footer from "@/components/Footer";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
 
-export default function HomePage({featuredProduct,newProducts}) {
+export default function HomePage({ featuredProduct, newProducts }) {
   return (
     <div>
       <Header />
       <Featured product={featuredProduct} />
       <NewProducts products={newProducts} />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const featuredProductId = '6549c30b03ea47afcd7406ef';
-  await mongooseConnect();
-  const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
-  return {
-    props: {
-      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-      newProducts: JSON.parse(JSON.stringify(newProducts)),
-    },
-  };
+  try {
+    const featuredProductId = '6549c30b03ea47afcd7406ef';
+    await mongooseConnect();
+    const featuredProduct = await Product.findById(featuredProductId);
+    const newProducts = await Product.find({}, null, { sort: { '_id': -1 }, limit: 10 });
+    
+    return {
+      props: {
+        featuredProduct: featuredProduct ? JSON.parse(JSON.stringify(featuredProduct)) : null,
+        newProducts: newProducts ? JSON.parse(JSON.stringify(newProducts)) : null,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        featuredProduct: null,
+        newProducts: null,
+      },
+    };
+  }
 }
